a65a2d655c6c04bd9d41f1474891141b2dfa8a7e
Challelign Tilahun <chalie2123@gmail.com>
2026-02-12 11:14:01 -0800

    14: Product list ui fix

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,10 +1,6 @@
 import { loadProductFilters } from "@/modules/products/search-params";
-import {
-  ProductList,
-  ProductListSkeleton,
-} from "@/modules/products/ui/components/product-list";
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
-import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { SearchParams } from "nuqs";
 
--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -3,6 +3,7 @@ import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import type { SearchParams } from "nuqs/server";
 import { loadProductFilters } from "@/modules/products/search-params";
 import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { DEFAULT_LIMIT } from "@/constants";
 
 interface Props {
   params: Promise<{
@@ -17,8 +18,16 @@ const CategoryPage = async ({ params, searchParams }: Props) => {
 
   console.log("filter", JSON.stringify(filters, null, 2));
   const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(
-    trpc.products.getMany.queryOptions({ ...filters, category }),
+  // void queryClient.prefetchQuery(
+  //   trpc.products.getMany.queryOptions({ ...filters, category }),
+  // );
+
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
+      ...filters,
+      category,
+      limit: DEFAULT_LIMIT,
+    }),
   );
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
